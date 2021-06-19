c89b9c9e739ead7b231b86a423bce08b6404bf09
diegoignacio01 <di.ibarrab@duocuc.cl>
2021-06-19 19:50:32 -0400

    Implementando las libreras de static en settings.py

--- TestDjango/settings.py
+++ TestDjango/settings.py
@@ -133,6 +133,8 @@ STATICFILES_DIRS =(
 )
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#implementacion de las librerias
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
